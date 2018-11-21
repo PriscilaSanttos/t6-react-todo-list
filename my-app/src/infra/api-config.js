76902455f72ec18420869ce4d97d01086b42dce0
Priscila Santtos <priscilapalmeira07@hotmail.com>
2018-11-21 16:59:51 -0200

    postit; apis

--- my-app/src/infra/api-config.js
+++ my-app/src/infra/api-config.js
@@ -1,14 +1,19 @@
 import axios from "axios"
+import { getUser } from "./local-storage";
 
 const config ={
     baseURL: "https://lehtodoapi.herokuapp.com/",
     timeout: 5000,
+    headers: ""
 }
 
 function api(){
-    config.headers = {
-        user: ""
-    } 
+    if(getUser()){
+        config.headers = {
+            user: getUser ()
+        }
+
+    }
     return  axios.create(config)
   }
 
--- my-app/src/pages/home/home.js
+++ my-app/src/pages/home/home.js
@@ -1,22 +1,45 @@
 import React from "react"
 import { Redirect } from "react-router-dom";
 import { getUser } from "../../infra/local-storage";
+import Postit from "../../components/postit";
+import {getPostitsApi} from "../../apis/postit.api"
+
 
 class Home extends React.Component{
     constructor (){
         super ()
+        this.state ={
+            postits : [
 
+            ]
+        }
     }
     componentDidMount (){
         console.log ("hello compenentDiMount foi criado")
+        this.getPostits()
     }
     componentWillUnmount (){
         console.log ("hello componentWillUnmount morreu")
     }
+    getPostits = () => {
+         getPostitsApi ()
+            .then((response) => {
+            console.log (response)
+            this.setState({
+                postits : response.data.todo
+            })
+         })
+        .catch((error) => {
+
+        })
+
+    }
     render (){
 
         if(getUser()){
-            return <div>Hello Home</div>
+            return this.state.postits.map((item) =>{
+                    return <Postit/>
+                })
         }else{
             return <Redirect to= "/login" />
         }
